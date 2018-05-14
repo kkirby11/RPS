876dc2c3e2a447356d160ae7995b36d0f4858ec5
kristina kirby <kristinakirby@mac.com>
2018-05-14 17:27:05 -0600

    rps

---
+++ js/main.js
@@ -0,0 +1,57 @@
+var x = 5;
+var y = 6;
+var z;
+debugger
+z = x + y;
+debuggery
+y = z;
+
+var rock = decodeURIComponent.getElementById('rock_picture')
+rock.addEventListener('click',function(){
+    playerChoice()
+})
+function playerChoice {
+
+}
+
+var paper = decodeURIComponent.getElementById('paper_picture')
+rock.addEventListener('click',function(){
+    playerChoice()
+})
+function playerChoice {
+    
+}
+
+var scissors = decodeURIComponent.getElementById('sicrs_picture')
+rock.addEventListener('click',function(){
+    playerChoice()
+})
+function playerChoice {
+    
+} 
+// creates images for placement?
+
+user.0.selection = 'left' ';
+    if (left) {
+        returns "You win";
+    } else {
+        returns "Nope Loser"
+    }
+    }
+    user.1.selection = 'center' ';
+    if (center) {
+        returns "You win";
+    } else {
+        returns "Nope Loser"
+    }
+    }
+    user.2.selection = 'right' ';
+    if (right) {
+        returns "You win";
+    } else {
+        returns "Nope Loser"
+    }
+    }
+
+</div>
+
